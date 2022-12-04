22ad457c3f4562de07f845a3f8ac1a652fe15c5c
ivan-petlya <104350431+ivan-petlya@users.noreply.github.com>
2022-12-04 19:11:21 +0200

    add styles

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,17 +3,15 @@ import css from './FeedbackOptions.module.css';
 export const FeedbackOptions = ({ handleGood, handleNeutral, handleBad }) => {
   return (
     <div className={css.buttonBox}>
-      <div className="Feedback__controls">
-        <button type="button" onClick={handleGood}>
-          Good
-        </button>
-        <button type="button" onClick={handleNeutral}>
-          Neutral
-        </button>
-        <button type="button" onClick={handleBad}>
-          Bad
-        </button>
-      </div>
+      <button type="button" className={css.good} onClick={handleGood}>
+        Good
+      </button>
+      <button type="button" className={css.neutral} onClick={handleNeutral}>
+        Neutral
+      </button>
+      <button type="button" className={css.bad} onClick={handleBad}>
+        Bad
+      </button>
     </div>
   );
 };
